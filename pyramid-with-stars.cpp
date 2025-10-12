c3331ee9b7f2313e8082f9bddd55a5c59bb6459e
ZayeemSVG <zayeemsavage123@gmail.com>
2025-10-12 16:25:20 +0600

    pyramid-with-stars.cpp

--- pyramid-with-stars.cpp
+++ pyramid-with-stars.cpp
@@ -4,13 +4,15 @@ int main(){
 
   int n=5;
      {
-        // code here
-        for(int i=1;i<=n;i++)
+        // using logic to build spaces when either less or greater than 
+       //the edge values of pyramid.j starts from 1 cause i used j as counter and 
+       //comparatively too.i=1 valid too but then n-i+1,,n+i-1 needed for comparison.
+        for(int i=0;i<n;i++)
         {
             for(int j=1;j<=((2*n)-1);j++)
             {
                 
-                if((j<(n-i+1))||(j>(n+i-1))){
+                if((j<(n-i))||(j>(n+i))){
                     cout<<" ";
                 }
                 
